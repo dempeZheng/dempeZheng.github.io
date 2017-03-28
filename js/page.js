bd66e83e369c573723995bf965aae5cf80b90ccc
DempeZheng <752996510@qq.com>
2017-03-28 20:42:53 +0800

    something

--- js/page.js
+++
@@ -1,6 +0,0 @@
-;(function(){
-  
-  var win_height = $(window).height()
-  $('.page').css('min-height', win_height - 50)
-  
-}())
--- js/post.js
+++
@@ -1,6 +0,0 @@
-;(function(){
-  
-  var win_height = $(window).height()
-  $('.post').css('min-height', win_height - 50)
-  
-}())
